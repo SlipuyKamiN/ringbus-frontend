679ed15d44a59caefc4dd2d1590181036e843edf
Mykhailo Fysiuk <mishayo53@gmail.com>
2023-09-28 11:24:54 +0200

    set: basic set up vars, bg, seo

---
+++ src/styles/common/vars.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primaryBrandColor: "#ffd800",
+  primaryWhite: "#ffffff",
+  primaryBlack: "#000000",
+  primaryWhiteAlpha: "rgba(#ffffff, 0.6)",
+  primaryBlackAlpha: "rgba(#000000, 0.6)",
+};
+
+export const transition = {
+  duration: "250ms cubic-bezier(0.4, 0, 0.2, 1)",
+};
